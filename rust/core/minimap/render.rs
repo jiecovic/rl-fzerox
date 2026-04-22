134e0bb822ad149e98d875113e8ceb5bc136f614
jiecovic <thomas.jerkovits@gmail.com>
2026-04-22 23:21:02 +0200

    perf(native): reduce bounds-heavy frame loops

--- rust/core/minimap/render.rs
+++ rust/core/minimap/render.rs
@@ -37,22 +37,29 @@ pub(super) fn render_layer_into(
         marker_layer.resize(roi_len, 0);
     }
 
-    for roi_y in 0..roi.height {
-        for roi_x in 0..roi.width {
-            let roi_index = roi_y * roi.width + roi_x;
-            let [red, green, blue] =
-                sample(roi.x + roi_x, roi.y + roi_y).ok_or(CoreError::NoFrameAvailable)?;
-            if mask[roi_index] == 0 {
-                roi_output[roi_index] = 0;
-                continue;
-            }
-            if is_player_marker_color(red, green, blue)
-                && let Some(marker_layer) = marker_layer.as_deref_mut()
-            {
-                marker_layer[roi_index] = 1;
-                marker_count += 1;
-            }
-            roi_output[roi_index] = TRACK_LUMA;
+    if let Some(marker_layer) = marker_layer.as_deref_mut() {
+        for (roi_y, ((mask_row, output_row), marker_row)) in mask
+            .chunks_exact(roi.width)
+            .zip(roi_output.chunks_exact_mut(roi.width))
+            .zip(marker_layer.chunks_exact_mut(roi.width))
+            .enumerate()
+        {
+            marker_count += render_roi_row(
+                roi,
+                roi_y,
+                mask_row,
+                output_row,
+                Some(marker_row),
+                &mut sample,
+            )?;
+        }
+    } else {
+        for (roi_y, (mask_row, output_row)) in mask
+            .chunks_exact(roi.width)
+            .zip(roi_output.chunks_exact_mut(roi.width))
+            .enumerate()
+        {
+            render_roi_row(roi, roi_y, mask_row, output_row, None, &mut sample)?;
         }
     }
 
@@ -106,15 +113,56 @@ fn rotate_luma_90_clockwise(layer: &[u8], width: usize, height: usize) -> (Vec<u
     let output_width = height;
     let output_height = width;
     let mut output = vec![0_u8; layer.len()];
-    for y in 0..height {
-        for x in 0..width {
-            let source_index = y * width + x;
+    for (y, row) in layer.chunks_exact(width).enumerate() {
+        for (x, value) in row.iter().enumerate() {
             let target_x = height - 1 - y;
             let target_y = x;
-            output[target_y * output_width + target_x] = layer[source_index];
+            output[target_y * output_width + target_x] = *value;
         }
     }
     (output, output_width, output_height)
+}
+
+fn render_roi_row(
+    roi: MinimapRoi,
+    roi_y: usize,
+    mask_row: &[u8],
+    output_row: &mut [u8],
+    marker_row: Option<&mut [u8]>,
+    sample: &mut impl FnMut(usize, usize) -> Option<[u8; 3]>,
+) -> Result<usize, CoreError> {
+    let mut marker_count = 0_usize;
+    match marker_row {
+        Some(marker_row) => {
+            for (roi_x, ((mask_value, output_value), marker_value)) in mask_row
+                .iter()
+                .zip(output_row.iter_mut())
+                .zip(marker_row.iter_mut())
+                .enumerate()
+            {
+                let [red, green, blue] =
+                    sample(roi.x + roi_x, roi.y + roi_y).ok_or(CoreError::NoFrameAvailable)?;
+                if *mask_value == 0 {
+                    *output_value = 0;
+                    continue;
+                }
+                if is_player_marker_color(red, green, blue) {
+                    *marker_value = 1;
+                    marker_count += 1;
+                }
+                *output_value = TRACK_LUMA;
+            }
+        }
+        None => {
+            for (roi_x, (mask_value, output_value)) in
+                mask_row.iter().zip(output_row.iter_mut()).enumerate()
+            {
+                sample(roi.x + roi_x, roi.y + roi_y).ok_or(CoreError::NoFrameAvailable)?;
+                *output_value = if *mask_value == 0 { 0 } else { TRACK_LUMA };
+            }
+        }
+    }
+    Ok(marker_count)
 }
 
 pub(super) fn write_zero_layer(
